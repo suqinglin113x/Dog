099d7e92cafa64df01f0a151dcba1e8e659d7db5
Lin <2413561757@qq.com>
2017-11-22 09:34:52 +0800

    Initial Commit

---
+++ Dog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dog
+//
+//  Created by Lin on 2017/11/22.
+//  Copyright © 2017年 国恒金服. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dog
+//
+//  Created by Lin on 2017/11/22.
+//  Copyright © 2017年 国恒金服. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
