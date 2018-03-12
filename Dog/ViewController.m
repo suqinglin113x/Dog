686efaf26d83fdd5394fe7bdf2d441df04be5093
苏 庆林 <2413561757@qq.com>
2018-03-12 13:24:05 +0800

    分支提交看看

--- Dog/ViewController.m
+++ Dog/ViewController.m
@@ -21,6 +21,7 @@
     // 写了点东西，
     // 不知所以然
     
+    // 在分支上提交点东西
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor redColor];
     NSArray *titles = @[@"你好，我是你阿爸爸", @"孙子，饿哦是"];
